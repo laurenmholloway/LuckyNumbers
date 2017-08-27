7f9d8690d9b31086bb721dbc5b3d186288f975c5
Lauren Holloway <laurenholloway@laurens-macbook.attlocal.net>
2017-08-26 23:18:39 -0400

    updating no repeats

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,12 +1,34 @@
+
+var whiteBalls = [];
+
 function generateWB(ball){
     var ball = document.getElementById(ball);
- 
+    ball.style.animation = "none";
     var randomNumber = Math.ceil(Math.random()*68);
-    ball.innerHTML = randomNumber;
+    var isABall = true;
+    while(isABall){
+        
+        if(whiteBalls.indexOf(randomNumber) != -1){
+            whiteBalls.push(randomNumber);
+            ball.innerHTML = randomNumber;
+            isABall = false;
+        }
+        else{
+            randomNumber = Math.ceil(Math.random()*68);
+        }
+        
+    }
+        
 }
 function generatePB(){
     var ball = document.getElementById('power');
-
+    ball.style.animation = "none";    
     var randomNumber = Math.ceil(Math.random()*25);
     ball.innerHTML = randomNumber;
+}
+
+function resetBalls(){
+    for(var ball in whiteBalls){
+        whiteBalls.pop();
+    }
 }
